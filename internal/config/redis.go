77cfcb3768a74dc9c918a171222b5fb02db2e3f1
hidayat <hidayathamir@gmail.com>
2026-02-06 06:43:39 +0700

    feat: redis retry

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/Hidayathamir/user-activity-tracking-go/pkg/constant/configkey"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
@@ -8,7 +10,9 @@ import (
 
 func NewRedis(viperConfig *viper.Viper) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: viperConfig.GetString(configkey.RedisAddress),
+		Addr:            viperConfig.GetString(configkey.RedisAddress),
+		MaxRetries:      3,
+		MinRetryBackoff: 8 * time.Millisecond,
 	})
 	return rdb
 }
